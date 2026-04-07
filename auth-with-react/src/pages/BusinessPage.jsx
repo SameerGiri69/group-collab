afc17c5104e142d71737e8b842aeafd21d240cf5
SameerGiri69 <sameergiri153000@gmail.com>
2026-04-07 23:14:03 +0545

    changes

--- auth-with-react/src/pages/BusinessPage.jsx
+++ auth-with-react/src/pages/BusinessPage.jsx
@@ -2,9 +2,11 @@
 import React from "react";
 
 export default function BusinessPage() {
-    return(
+    return (
         <>
-        <h1>Welcome to the business page</h1>
+            <h1>Welcome to the business page</h1>
+
+            <h2>Ayush dahal le 500 ma chak bechyo re ta</h2>
         </>
     )
 }
