3c8557abde4d8972a27dc15a3355f4d7082e6bb0
Ayushdahal12 <138177510+Ayushdahal12@users.noreply.github.com>
2026-04-07 23:29:23 +0545

    both pages

--- auth-with-react/src/App.jsx
+++ auth-with-react/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Dhedu from './assets/Component/Dhedu'
+import { Routes, Route } from "react-router-dom";
+import Dhedu from "./assets/Component/Dhedu";
+import BusinessPage from "./Pages/BusinessPage"; 
 
-const App = () => {
+
+function App() {
   return (
     <>
-      <Dhedu/>
+
+      <Routes>
+        <Route path="/" element={<Dhedu />} />
+        <Route path="/business" element={<BusinessPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
